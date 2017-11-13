101786628fc6a26941dd6a395ee370bd210c5519
Jeroen <jeroen.roos8@hotmail.com>
2017-11-13 22:21:33 +0100

    First commit

---
+++ 554052_ZorgVoorHetHart/554052_ZorgVoorHetHart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  554052_ZorgVoorHetHart
+//
+//  Created by Jeroen on 13/11/2017.
+//  Copyright © 2017 Jeroen Roos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
