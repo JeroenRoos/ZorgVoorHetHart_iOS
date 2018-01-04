b8f8f1a814fe27a53b0d8a47f917cafbe51066ef
Jeroen <jeroen.roos8@hotmail.com>
2018-01-04 16:08:56 +0100

    Removed Big Text from Service

--- 554052_ZorgVoorHetHart/554052_ZorgVoorHetHart/ViewControllers/Service/MyServiceHomeViewController.swift
+++ 554052_ZorgVoorHetHart/554052_ZorgVoorHetHart/ViewControllers/Service/MyServiceHomeViewController.swift
@@ -33,11 +33,11 @@ class MyServiceHomeViewController: UIViewController, UITextFieldDelegate
     @IBOutlet weak var switchWeeklyMeasurement: UISwitch!
     
     @IBOutlet weak var txtMyAccount: UILabel!
-    @IBOutlet weak var switchBigText: UISwitch!
+    //@IBOutlet weak var switchBigText: UISwitch!
     @IBOutlet weak var txtMySettings: UILabel!
     @IBOutlet var myView: UIView!
-    @IBOutlet weak var imgBigText: UIImageView!
-    @IBOutlet weak var txtBigText: UILabel!
+    //@IBOutlet weak var imgBigText: UIImageView!
+    //@IBOutlet weak var txtBigText: UILabel!
     
     @IBOutlet weak var txtWeightHeight: UILabel!
     @IBOutlet weak var imgWeightHeight: UIImageView!
@@ -79,37 +79,37 @@ class MyServiceHomeViewController: UIViewController, UITextFieldDelegate
         txtDisclaimerTitle.text = "Disclaimer"
         txtDisclaimerTitle.font = UIFont(name:"HelveticaNeue-Bold", size: 15.0)
         
-        imgBigText.backgroundColor = UIColor(rgb: 0xF8F8F8)
-        txtBigText.text = "Grotere tekst"
-        txtBigText.font = txtBigText.font.withSize(12)
-        switchBigText.setOn(defaults.bool(forKey: "bigText"), animated: false)
-        switchDailyNotification.addTarget(self, action: #selector(switchBigTextChanged(_:)), for: .valueChanged)
+        //imgBigText.backgroundColor = UIColor(rgb: 0xF8F8F8)
+        //txtBigText.text = "Grotere tekst"
+        //txtBigText.font = txtBigText.font.withSize(12)
+        //switchBigText.setOn(defaults.bool(forKey: "bigText"), animated: false)
+        //switchDailyNotification.addTarget(self, action: #selector(switchBigTextChanged(_:)), for: .valueChanged)
         
         imgDailyNotification.backgroundColor = UIColor(rgb: 0xF8F8F8)
         txtDailyNotification.text = "Dagelijkse herinneringen voor uw metingen"
-        txtDailyNotification.font = txtBigText.font.withSize(12)
+        txtDailyNotification.font = txtDailyNotification.font.withSize(12)
         switchDailyNotification.setOn(defaults.bool(forKey: "dailyNotifications"), animated: false)
         switchDailyNotification.addTarget(self, action: #selector(switchDailyNotificationChanged(_:)), for: .valueChanged)
         
         imgWeeklyMeasurement.backgroundColor = UIColor(rgb: 0xF8F8F8)
         txtWeeklyMeasurement.text = "Weekelijks uw metingen opsturen naar uw consulent"
-        txtWeeklyMeasurement.font = txtBigText.font.withSize(12)
+        txtWeeklyMeasurement.font = txtWeeklyMeasurement.font.withSize(12)
         switchWeeklyMeasurement.setOn(defaults.bool(forKey: "sendWeeklyMeasurement"), animated: false)
         switchWeeklyMeasurement.addTarget(self, action: #selector(switchWeeklyMeasurementChanged(_:)), for: .valueChanged)
         
         imgWeightHeight.backgroundColor = UIColor(rgb: 0xF8F8F8)
         txtWeightHeight.text = "Uw lengte en gewicht aanpassen"
-        txtWeightHeight.font = txtBigText.font.withSize(12)
+        txtWeightHeight.font = txtWeightHeight.font.withSize(12)
         
         imgAutomaticLogin.backgroundColor = UIColor(rgb: 0xF8F8F8)
         txtAutomaticLogin.text = "Automatisch inloggen"
-        txtAutomaticLogin.font = txtBigText.font.withSize(12)
+        txtAutomaticLogin.font = txtAutomaticLogin.font.withSize(12)
         switchAutomaticLogin.setOn(defaults.bool(forKey: "automaticLogin"), animated: false)
         switchAutomaticLogin.addTarget(self, action: #selector(switchAutomaticLoginChanged(_:)), for: .valueChanged)
         
         imgLogout.backgroundColor = UIColor(rgb: 0xF8F8F8)
         txtLogout.text = "Uitloggen"
-        txtLogout.font = txtBigText.font.withSize(12)
+        txtLogout.font = txtLogout.font.withSize(12)
         
         txtDisclaimerInfo.text = "Deze app is met de grootst mogelijke zorgvuldigheid samengesteld. Wij kunnen echter niet garanderen dat de app altijd zonder onderbrekingen, fouten of gebreken beschikbaar zal zijn of werken en dat de verschafte informatie volledig, juist of up-to-date is."
         txtDisclaimerInfo.backgroundColor = UIColor(rgb: 0xF8F8F8)
@@ -259,11 +259,11 @@ class MyServiceHomeViewController: UIViewController, UITextFieldDelegate
         btnPopupRight.isHidden = value
     }
     
-    @objc func switchBigTextChanged(_ mySwitch: UISwitch)
-    {
-        let value = mySwitch.isOn
-        defaults.set(value, forKey: "bigText")
-    }
+    //@objc func switchBigTextChanged(_ mySwitch: UISwitch)
+    //{
+    //    let value = mySwitch.isOn
+    //    defaults.set(value, forKey: "bigText")
+    //}
     
     @objc func switchDailyNotificationChanged(_ mySwitch: UISwitch)
     {
